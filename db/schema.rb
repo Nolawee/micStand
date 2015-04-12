fd2878bd6641c75acd1e50726eeb3ba582fdb626
Nolawee Mengist <nome9478@colorado.edu>
2015-04-12 03:48:32 -0600

    Review Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411151601) do
+ActiveRecord::Schema.define(version: 20150412091818) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "specials", force: :cascade do |t|
     t.string   "title"
